a9c59faf30c2ca02fd2a3ddd13a44c9ac49882de
YANG Jing <jyang@thoughtworks.com>
2015-08-03 10:40:16 +0800

    [JingYang] find missing steps back.

--- RubyCucumberDemo/features/step_definitions/details_steps.rb
+++ RubyCucumberDemo/features/step_definitions/details_steps.rb
@@ -5,3 +5,7 @@ end
 假如(/^:我进入"([^"]*)"款产品的详细页面$/) do |number|
 	App.details_page.load(product_number: number)
 end
+
+而且(/^:我选择以下套餐加入购物车:$/) do |table|
+   App.details_page.select_package table
+end
